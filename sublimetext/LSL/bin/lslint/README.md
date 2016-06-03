bc1e4be4bd51cad90028e1793934f08483851ebd
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2016-06-03 11:49:03 +0200

    Simplify structure for Sublime Text (#12)
    * Update LSL.sublime-build
    * Update and rename sublimetext/LSL/bin/LICENSE.md to sublimetext/LSL/bin/lslint/LICENSE.md
    * Delete lslint
    * Delete lslint
    * Delete lslint.exe
    * Rename LICENSE.md to README.md
    * Create .gitkeep
    * Create .gitkeep
    * Create .gitkeep
    * Add files via upload
    * Delete .gitkeep
    * Add files via upload
    * Add files via upload
    * Delete .gitkeep
    * Delete .gitkeep
    * Update LSL.sublime-build
    * Update main.py
    * Rename sublimetext/LSL/color-schemes/LSL.hidden-tmTheme to sublimetext/LSL/other/LSL.hidden-tmTheme
    * Delete settings_spellcheck.tmPreferences
    * Delete settings_symbols_event.tmPreferences
    * Delete settings_symbols_function.tmPreferences
    * Delete settings_symbols_state.tmPreferences
    * Rename settings_general.tmPreferences to LSL.tmPreferences
    * Update LSL.tmPreferences
    * Update LSL.tmPreferences
    * Update LSL_indent_style.tmPreferences.allman
    * Update and rename LSL_indent_style.tmPreferences.kandr to LSL_indent_style.tmPreferences.k_and_r
    * Update lsl.constants.sublime-completions
    * Update lsl.events.sublime-completions
    * Update Main.sublime-menu
    * Update LSL.sublime-settings
    * Update LSL.sublime-syntax
    * Update LSL.hidden-tmTheme

---
+++ sublimetext/LSL/bin/lslint/README.md
@@ -0,0 +1,10 @@
+## LICENSE:
+
+All code is public domain unless otherwise noted.
+
+## SOURCE:
+
+LSLint files have been taken from:
+
+* [makopo/lslint](https://github.com/makopo/lslint/)
+* [pclewis/lslint](https://github.com/pclewis/lslint/)
--- sublimetext/LSL/main.py
+++ sublimetext/LSL/main.py
@@ -5,15 +5,16 @@ import stat
 import sublime
 import sublime_plugin
 
-SETTINGS_FILE = 'LSL.sublime-settings'
-INDENT_STYLE = 'LSL_indent_style.tmPreferences'
-INDENT_STYLE_ALLMAN = 'LSL_indent_style.tmPreferences.allman'
-INDENT_STYLE_KANDR = 'LSL_indent_style.tmPreferences.kandr'
+settings = None
+SETTINGS_FILE = None
+INDENT_STYLE = None
+INDENT_STYLE_ALLMAN = None
+INDENT_STYLE_K_AND_R = None
 
 def chmod_lslint():
-    binaryPath = os.path.join(sublime.packages_path(), 'LSL/bin/osx/lslint')
+    binaryPath = os.path.join(sublime.packages_path(), 'LSL/bin/lslint/osx/lslint')
     os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
-    binaryPath = os.path.join(sublime.packages_path(), 'LSL/bin/linux/lslint')
+    binaryPath = os.path.join(sublime.packages_path(), 'LSL/bin/lslint/linux/lslint')
     os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
 
 def set_default_lsl_indent_style():
@@ -24,10 +25,21 @@ def set_default_lsl_indent_style():
         shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_ALLMAN), destfile)
 
 if sublime.version().startswith('2'):
-    chmod_lslint()
-    set_default_lsl_indent_style()
+    plugin_loaded()
 
 def plugin_loaded():
+    global settings
+    global SETTINGS_FILE
+    global INDENT_STYLE
+    global INDENT_STYLE_ALLMAN
+    global INDENT_STYLE_K_AND_R
+
+    SETTINGS_FILE = 'LSL.sublime-settings'
+    settings = sublime.load_settings(SETTINGS_FILE)
+    INDENT_STYLE = 'LSL_indent_style.tmPreferences'
+    INDENT_STYLE_ALLMAN = 'LSL_indent_style.tmPreferences.allman'
+    INDENT_STYLE_K_AND_R = 'LSL_indent_style.tmPreferences.k_and_r'
+
     chmod_lslint()
     set_default_lsl_indent_style()
 
@@ -35,16 +47,14 @@ class ChangeEditorSchemeCommand(sublime_plugin.WindowCommand):
     _is_checked = False
 
     def __init__(self, view):
-        settings = sublime.load_settings(SETTINGS_FILE)
         self._is_checked = settings.has('color_scheme')
 
     def run(self):
-        settings = sublime.load_settings(SETTINGS_FILE)
         if self._is_checked:
             settings.erase('color_scheme')
             self._is_checked = False
         else:
-            settings.set('color_scheme', 'Packages/LSL/color-schemes/LSL.hidden-tmTheme')
+            settings.set('color_scheme', 'Packages/LSL/other/LSL.hidden-tmTheme')
             self._is_checked = True
         sublime.save_settings(SETTINGS_FILE)
 
@@ -53,8 +63,8 @@ class ChangeEditorSchemeCommand(sublime_plugin.WindowCommand):
 
 class ChangeStyleCommand(sublime_plugin.WindowCommand):
     '''
-        Changing the indent style is only being done via the *.tmPreferences
-        files, not via the LSL.sublime-settings file.
+        Changing the indent style is only being done via the
+        *.tmPreferences files, not via the LSL.sublime-settings file.
     '''
 
     _is_checked = False
@@ -66,29 +76,17 @@ class ChangeStyleCommand(sublime_plugin.WindowCommand):
         if os.path.exists(destfile):
             pl = plistlib.readPlist(destfile)
             self._is_checked = (pl['name'] == 'Indent Style - K & R')
-#       else:
-#           shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_ALLMAN), destfile)
-#       settings = sublime.load_settings(SETTINGS_FILE)
-#       if self._is_checked:
-#           settings.set('lsl-indent-style', 'kandr')
-#       else:
-#           settings.erase('lsl-indent-style')
-#       sublime.save_settings(SETTINGS_FILE)
 
     def run(self):
         srcdir = os.path.join(sublime.packages_path(), 'LSL/metadata')
         destdir = os.path.join(sublime.packages_path(), 'User')
         destfile = os.path.join(destdir, INDENT_STYLE)
-#       settings = sublime.load_settings(SETTINGS_FILE)
         if self._is_checked:
             shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_ALLMAN), destfile)
-#           settings.erase('lsl-indent-style')
             self._is_checked = False
         else:
-            shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_KANDR), destfile)
-#           settings.set('lsl-indent-style', 'kandr')
+            shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_K_AND_R), destfile)
             self._is_checked = True
-#       sublime.save_settings(SETTINGS_FILE)
 
     def is_checked(self):
         return self._is_checked
