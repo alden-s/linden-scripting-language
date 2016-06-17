f85bf5732ce38ecbb5f817d4d3daa9fd6d4caafe
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2016-06-17 14:46:45 +0200

    Make sublime-package and add link to SublimeLinter (#16)
    * Delete .no-sublime-package
    * Update main.py
    * Delete README.md
    * Delete lslint
    * Delete lslint
    * Delete lslint.exe
    * Delete LSL.sublime-build
    * Update Main.sublime-menu
    * Update README.md

--- sublimetext/LSL/main.py
+++ sublimetext/LSL/main.py
@@ -1,11 +1,7 @@
 import os
 import plistlib
-import re
-import shutil
-import stat
 import sublime
 import sublime_plugin
-import sys
 from time import time
 import webbrowser
 import xml.etree.ElementTree as etree
@@ -35,9 +31,9 @@ def plugin_loaded():
     global INDENT_STYLE_K_AND_R
 
     SETTINGS_FILE = 'LSL.sublime-settings'
-    INDENT_STYLE = 'LSL_indent_style.tmPreferences'
-    INDENT_STYLE_ALLMAN = 'LSL_indent_style.tmPreferences.allman'
-    INDENT_STYLE_K_AND_R = 'LSL_indent_style.tmPreferences.k_and_r'
+    INDENT_STYLE = os.path.join(sublime.packages_path(), 'User', 'LSL_indent_style.tmPreferences')
+    INDENT_STYLE_ALLMAN = sublime.load_resource('Packages/LSL/metadata/LSL_indent_style.tmPreferences.allman')
+    INDENT_STYLE_K_AND_R = sublime.load_resource('Packages/LSL/metadata/LSL_indent_style.tmPreferences.k_and_r')
 
     class Pref:
         def load(self):
@@ -53,19 +49,12 @@ def plugin_loaded():
 
     settings.add_on_change('reload', lambda:Pref.load())
 
-    binaryPath = os.path.join(sublime.packages_path(), 'LSL', 'bin', 'lslint', 'osx', 'lslint')
-    os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
-    binaryPath = os.path.join(sublime.packages_path(), 'LSL', 'bin', 'lslint', 'linux', 'lslint')
-    os.chmod(binaryPath, stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
+    if not os.path.exists(INDENT_STYLE):
+        with open(INDENT_STYLE, mode='w', newline='\n') as file:
+            file.write(INDENT_STYLE_ALLMAN)
 
-    srcdir = os.path.join(sublime.packages_path(), 'LSL', 'metadata')
-    destdir = os.path.join(sublime.packages_path(), 'User')
-    destfile = os.path.join(destdir, INDENT_STYLE)
-    if not os.path.exists(destfile):
-        shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_ALLMAN), destfile)
-
-    kwdbElementTree = etree.parse(os.path.join(sublime.packages_path(), 'LSL', 'other', 'kwdb', 'kwdb.xml'))
-    KWDB = kwdbElementTree.getroot()
+    kwdbAsString = sublime.load_resource('Packages/LSL/other/kwdb/kwdb.xml')
+    KWDB = etree.fromstring(kwdbAsString)
 
 class ChangeEditorSchemeCommand(sublime_plugin.WindowCommand):
 
@@ -79,10 +68,9 @@ class ChangeEditorSchemeCommand(sublime_plugin.WindowCommand):
 
         if self._is_checked:
             settings.erase('color_scheme')
-            self._is_checked = False
         else:
             settings.set('color_scheme', 'Packages/LSL/other/LSL.hidden-tmTheme')
-            self._is_checked = True
+        self._is_checked = not self._is_checked
 
         sublime.save_settings(SETTINGS_FILE)
 
@@ -96,41 +84,24 @@ class ChangeStyleCommand(sublime_plugin.WindowCommand):
 
     def __init__(self, view):
 
-        srcdir = os.path.join(sublime.packages_path(), 'LSL', 'metadata')
-        destdir = os.path.join(sublime.packages_path(), 'User')
-        destfile = os.path.join(destdir, INDENT_STYLE)
-
-        if os.path.exists(destfile):
-            pl = plistlib.readPlist(destfile)
+        if os.path.exists(INDENT_STYLE):
+            pl = plistlib.readPlist(INDENT_STYLE)
             self._is_checked = (pl['name'] == 'Indent Style - K & R')
 
     def run(self):
 
-        srcdir = os.path.join(sublime.packages_path(), 'LSL', 'metadata')
-        destdir = os.path.join(sublime.packages_path(), 'User')
-        destfile = os.path.join(destdir, INDENT_STYLE)
-
         if self._is_checked:
-            shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_ALLMAN), destfile)
-            self._is_checked = False
+            with open(INDENT_STYLE, mode='w', newline='\n') as file:
+                file.write(INDENT_STYLE_ALLMAN)
         else:
-            shutil.copyfile(os.path.join(srcdir, INDENT_STYLE_K_AND_R), destfile)
-            self._is_checked = True
+            with open(INDENT_STYLE, mode='w', newline='\n') as file:
+                file.write(INDENT_STYLE_K_AND_R)
+
+        self._is_checked = not self._is_checked
 
     def is_checked(self):
 
         return self._is_checked
-
-class OpenLslDefaultSettingsCommand(sublime_plugin.WindowCommand):
-
-    def run(self):
-        settings_read_only = sublime.load_resource('Packages/LSL/settings/LSL.sublime-settings')
-        v = sublime.active_window().new_file()
-        v.run_command('append', {'characters': settings_read_only})
-        v.set_name('LSL Settings - Default (read-only)')
-        v.set_syntax_file('Packages/JavaScript/JSON.sublime-syntax')
-        v.set_scratch(True)
-        v.set_read_only(True)
 
 class LslCommand(sublime_plugin.EventListener):
 
--- sublimetext/README.md
+++ sublimetext/README.md
@@ -1,3 +1,7 @@
 ### Usage
 
-Be advised to upgrade to [Sublime Text 3](https://www.sublimetext.com/3) in case you haven't already.
+* Make sure you are running [Sublime Text](https://www.sublimetext.com) **Build 3070 or later** to get Tooltips (via `sublime.view.show_popup()`) running.
+* The contents of the [`LSL`](LSL) subfolder should be zipped, the zipfile renamed to `LSL.sublime-package` and dropped into `./Data/Installed Packages` where [Sublime Text](https://www.sublimetext.com) is installed.
+* Your advised to use the following [Sublime Text](https://www.sublimetext.com) packages:
+  * [SublimeLinter](https://packagecontrol.io/packages/SublimeLinter)
+  * [SublimeLinter-contrib-lslint](https://packagecontrol.io/packages/SublimeLinter-contrib-lslint)
