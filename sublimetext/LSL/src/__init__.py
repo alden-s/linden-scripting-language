b8d1fcac65b7963100e519c340ca1fb15134af84
Builder's Brewery <buildersbrewery@users.noreply.github.com>
2017-03-03 02:29:34 +0100

    Update __init__.py

--- sublimetext/LSL/src/__init__.py
+++ sublimetext/LSL/src/__init__.py
@@ -2,5 +2,5 @@ __pkg_name__ = 'LSL'
 
 from .pc_events_notifier import *
 from .sublimelinter_contrib_lslint import *
-from .text_commands import *
 from .tooltips import *
+from .window_commands import *
